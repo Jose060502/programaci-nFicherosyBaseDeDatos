669bb91da41641a8462ec9a9b07fb50d13513e4d
Jose <jseda442@gmail.com>
2024-04-10 08:56:48 +0200

    h

---
+++ BoletinFicheros/src/EjercicioF1.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class EjercicioF1 {
+    public static void main(String[] args){
+        File archivoNuevo = new File(".\\src\\FicheroEj1.txt");
+        try(BufferedReader br = new BufferedReader(new FileReader(archivoNuevo))) {
+            int cont = 0;
+            while (br.readLine() != null){
+                cont++;
+            }
+            System.out.println("El fichero tiene estas "+ cont+" lineas");
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
